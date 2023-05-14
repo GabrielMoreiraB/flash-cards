57bda30a825fafd9dbddc621f853a64fc3def55d
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-05-14 16:47:41 -0300

    .

---
+++ react-flash-cards-v1-aula-lifting-state-up/src/services/apiService.js
@@ -0,0 +1,8 @@
+import { get } from "./httpServices";
+
+const BACKAND_URL = 'http://localhost:3001/flashcards'
+
+export async function apiGetAllFlashCards() {
+    const allFlashCards = await get(BACKAND_URL);
+    return allFlashCards
+}
